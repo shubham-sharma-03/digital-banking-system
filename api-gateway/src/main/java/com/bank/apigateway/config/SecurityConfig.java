abfa3d3a371fca6c19dbeefc1956e838fa83b803
shubham-sharma-03 <sharmashubham05275@gmail.com>
2026-07-19 21:18:31 +0530

    fix

--- api-gateway/src/main/java/com/bank/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/bank/apigateway/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(Arrays.asList(
+                "https://spectacular-optimism-production-55b6.up.railway.app",
                 "https://account-service-no0u.onrender.com",
                 "http://localhost:5500",
                 "http://127.0.0.1:5500",
@@ -45,10 +46,7 @@ public class SecurityConfig {
         ));
 
         configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setExposedHeaders(Arrays.asList(
-                "Authorization",
-                "Content-Type"
-        ));
+        configuration.setExposedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
 
